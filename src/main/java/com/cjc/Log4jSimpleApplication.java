f6372abbdd5e6a2759fb5cc97e8a6bb04a4f2df6
jaybhoirkar123 <55053020+jaybhoirkar123@users.noreply.github.com>
2019-09-16 11:14:34 +0530

    bye

--- src/main/java/com/cjc/Log4jSimpleApplication.java
+++ src/main/java/com/cjc/Log4jSimpleApplication.java
@@ -9,6 +9,7 @@ public class Log4jSimpleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Log4jSimpleApplication.class, args);
 		System.out.println("sp");
+		System.out.println("sp");
 		
 		
 	}
